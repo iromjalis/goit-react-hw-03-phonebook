4392caf9f0cc93818268977473923f8beb2efe08
Irina Romjalis <iromjalis@gmail.com>
2021-09-11 01:25:56 +0200

    state +function for form + filter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
 import { Component } from "react";
 import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import Container from "./components/Container/Container";
+import Filter from "./components/Filter/Filter";
 
-function App() {
-  return <div className="App">goit-react-hw-03-phonebook</div>;
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+    name: "",
+    number: "",
+  };
+  handleSubmit = (data) => {
+    this.setState({ ...data });
+  };
+  handleFilterChange = (filter) => {
+    this.setState({ ...filter });
+  };
+  render() {
+    const { name, number, filter, contacts } = this.state;
+    const filtered = contacts.map((contact) => contact.name === filter);
+    return (
+      <>
+        <Container className="App">
+          <h1>Phonebook</h1>
+          <ContactForm
+            name={name}
+            number={number}
+            handleSubmit={this.handleSubmit}
+          />
+          <h2>Contacts</h2>
+          <Filter
+            filter={filter}
+            handleFilterChange={this.handleFilterChange}
+          />
+          <ContactList contacts={contacts} />
+        </Container>
+      </>
+    );
+  }
 }
 
 export default App;
