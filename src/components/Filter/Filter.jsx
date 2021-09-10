0bd9a1246cf968b31519e4b6507758ecf5af0c58
Irina Romjalis <iromjalis@gmail.com>
2021-09-11 01:25:13 +0200

    filter adding to App state (no function)

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
+//import { Test } from './Filter.styles';
+
+class Filter extends Component {
+  state = {
+    filter: "",
+  };
+  handleFilter = (e) => {
+    this.setState({ filter: e.target.value });
+    this.props.handleFilterChange(this.state);
+  };
+  render() {
+    const { handleFilterChange } = this.props;
+    return (
+      <div className="FilterWrapper">
+        <form action="">
+          <label htmlFor="">
+            <p> Find contactas by name</p>
+            <input
+              onChange={this.handleFilter}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+            />
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+Filter.propTypes = {
+  // bla: PropTypes.string,
+};
+
+Filter.defaultProps = {
+  // bla: 'test',
+};
+
+export default Filter;
