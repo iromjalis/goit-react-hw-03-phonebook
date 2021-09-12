97c3f9c690c4756955b49f0fcc39a2cd61e3a1b3
Irina Romjalis <iromjalis@gmail.com>
2021-09-12 18:47:14 +0200

    filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,36 +2,27 @@ import React, { Component } from "react";
 // import PropTypes from "prop-types";
 //import { Test } from './Filter.styles';
 
-class Filter extends Component {
-  state = {
-    filter: "",
-  };
-  handleFilter = (e) => {
-    this.setState({ filter: e.target.value });
-    this.props.handleFilterChange(this.state);
-  };
-  render() {
-    const { handleFilterChange } = this.props;
-    return (
-      <div className="FilterWrapper">
-        <form action="">
-          <label htmlFor="">
-            <p> Find contacts by name</p>
-            <input
-              placeholder="Find contacts"
-              onChange={this.handleFilter}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-              required
-            />
-          </label>
-        </form>
-      </div>
-    );
-  }
-}
+const Filter = ({ value, handleFilterChange }) => {
+  return (
+    <div className="FilterWrapper">
+      <form action="">
+        <label htmlFor="">
+          <p> Find contacts by name</p>
+          <input
+            placeholder="Find contacts"
+            onChange={handleFilterChange}
+            type="text"
+            name="name"
+            value={value}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+          />
+        </label>
+      </form>
+    </div>
+  );
+};
 
 Filter.propTypes = {
   // bla: PropTypes.string,
