b2682d8b9d59c4d550f8b55b4a99bf9372b92b4c
Irina Romjalis <iromjalis@gmail.com>
2021-09-12 18:54:20 +0200

    setItem + getItem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import shortid from "shortid";
 import "./App.css";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
@@ -13,28 +14,60 @@ class App extends Component {
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
-    Filter: "",
-    Name: "",
-    Number: "",
+    filter: "",
+    name: "",
+    number: "",
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem("contacts");
+    if (contacts) {
+      this.setState({ contacts: JSON.parse(contacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(contacts));
+    }
+  }
+
   handleSubmit = (data) => {
     this.setState({ ...data });
+    this.setState({ name: "", number: "" });
   };
-  handleFilterChange = (filter) => {
-    this.setState({ ...filter });
+  handleFilterChange = (e) => {
+    this.setState({ filter: e.currentTarget.value });
   };
   handleDeleteContact = (contactId) => {
-    console.log(contactId);
     this.setState((prevState) => ({
       contacts: prevState.contacts.filter(
         (contact) => contact.id !== contactId
       ),
     }));
   };
+  handleAddContact = (data) => {
+    const newContact = {
+      id: shortid.generate(),
+      ...data,
+    };
+
+    if (this.state.contacts.some(({ name }) => name === data.name)) {
+      alert(`${data.name} already exists`);
+      return;
+    }
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+    this.setState({ name: "", number: "" });
+  };
 
   render() {
     const { name, number, filter, contacts } = this.state;
-    const filtered = contacts.map((contact) => contact.name === filter);
+    const filtered = contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
     return (
       <>
         <Container className="App">
@@ -43,14 +76,12 @@ class App extends Component {
             name={name}
             number={number}
             handleSubmit={this.handleSubmit}
+            onSubmit={this.handleAddContact}
           />
           <h2>Contacts</h2>
-          <Filter
-            filter={filter}
-            handleFilterChange={this.handleFilterChange}
-          />
+          <Filter value={filter} handleFilterChange={this.handleFilterChange} />
           <ContactList
-            contacts={contacts}
+            contacts={filtered}
             handleDeleteContact={this.handleDeleteContact}
           />
         </Container>
